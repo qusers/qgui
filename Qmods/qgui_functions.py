ad110f689fc6946fc4625630512a24a2e1b959b4
isaksengeir <geir.isaksen@uit.no>
2016-11-02 14:38:11 +0100

    Added qgui_functions.py. Will collect common functions in these file shared by the different classes.

---
+++ Qmods/qgui_functions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# This file is part of Qgui.
+
+# Qgui source file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Qgui is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Qgui.  If not, see <http://www.gnu.org/licenses/>.
+
+__description__ = \
+    """
+Collection of functions commonly used by the different Qgui Classes.
+"""
+
+import numpy as np
+import sys
+import re
+
+def read_topolgy(top,libfiles):
+    """
+    :param top: a topolgy file (*.top)
+    :param libfiles: a list with absolute paths to library files in settings
+    :return: dictionary with {residue nr: residue}
+    """
+    pass
