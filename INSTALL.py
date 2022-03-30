16603487957b62c0c1e63656efce8b30eadbca44
isaksengeir <geir.isaksen@uit.no>
2022-03-30 13:38:14 +0200

    Can not use venv because it causes pymol not to work :(x

--- INSTALL.py
+++ INSTALL.py
@@ -51,8 +51,8 @@ if not os.path.isdir('%s/Qmods' % install_path):
     os.makedirs('%s/Qmods' % install_path)
 
 org_path = os.path.dirname(os.path.realpath(__file__))
-shutil.copy2('%s/qgui.py' % org_path, '%s/qgui.py' % install_path)
-shutil.copy2('%s/Qgui' % org_path, '%s/Qgui' % install_path)
+shutil.copy2('%s/qgui.py' % org_path, '%s/Qgui' % install_path)
+#shutil.copy2('%s/Qgui' % org_path, '%s/Qgui' % install_path)
 shutil.copy2('%s/requirements.txt' % org_path, '%s/requirements.txt' % install_path)
 
 for f in os.listdir('Qmods'):
@@ -104,7 +104,10 @@ else:
     print('%s updated' % bashfile)
 
 #Make venv and install requirements
-venv_activate = "python3 -m venv %s/venv; . %s/venv/bin/activate; pip install -r %s/requirements.txt" % (install_path, install_path, install_path)
+#venv_activate = "python3 -m venv %s/venv; . %s/venv/bin/activate; pip install -r %s/requirements.txt" % (install_path, install_path, install_path)
+
+#install requirements
+reqs = "python3 -m pip install -r %s/requirements.txt" % install_path
 
 ret = subprocess.run(venv_activate, capture_output=True, shell=True)
 print(ret.stdout.decode())
